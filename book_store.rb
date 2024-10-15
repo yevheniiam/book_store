4c8b127632453fb69e86bb531b7f6a0b117b5993
Alena Cherniaieva <122041607+Alena011@users.noreply.github.com>
2024-10-15 11:02:26 -0700

    Update book_store.rb

--- book_store.rb
+++ book_store.rb
@@ -6,3 +6,12 @@ class Shelf
     @position = position
   end
 end
+# Клас для опису товару - алена
+class Item
+  attr_accessor :id, :popularity
+
+  def initialize(id, popularity)
+    @id = id
+    @popularity = popularity
+  end
+end
